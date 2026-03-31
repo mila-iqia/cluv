e230fe5b3899104c3669a47e17654ad8df426d89
Fabrice Normandin <normandf@mila.quebec>
2026-03-31 17:14:30 -0400

    Update the example job script and pyproject.toml
    Signed-off-by: Fabrice Normandin <normandf@mila.quebec>

--- scripts/job.sh
+++ scripts/job.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #SBATCH --job-name=cluv-test
-#SBATCH --output=slurm-%j.out
+#SBATCH --output=logs/slurm-%j.out
 #SBATCH --ntasks=1
 #SBATCH --time=0:05:00
+
+project_root="$HOME/repos/cluv"
 
 # Minimal test job for cluv submit integration tests.
 echo "hostname: $(hostname)"
 echo "GIT_COMMIT=${GIT_COMMIT:?GIT_COMMIT is not set. Use 'cluv submit' to submit this job script.}"
 # echo "Job has been restarted $SLURM_RESTART_COUNT times."
-
-project_root="$HOME/repos/cluv"
 
 # Setup the repo in $SLURM_TMPDIR, so the code can change in the project without affecting the job.
 srun --ntasks-per-node=1 --ntasks=$SLURM_NNODES --input=all bash -e <<END
@@ -22,4 +22,4 @@ END
 srun uv --dir=$SLURM_TMPDIR/cluv run "$@"
 
 # Copy results (if any) from the local storage back to the results dir (eg in $SCRATCH)
-srun --ntasks-per-node=1 rsync --update --recursive $SLURM_TMPDIR/cluv/results $project_root/results
+srun --ntasks-per-node=1 rsync --update --recursive $SLURM_TMPDIR/cluv/logs $project_root/logs

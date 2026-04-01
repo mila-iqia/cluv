89cdeb7ec3e435bd219ba6a46db348c54d5b6692
Fabrice Normandin <normandf@mila.quebec>
2026-04-01 17:55:32 -0400

    Use --chdir so logs land in right place
    Signed-off-by: Fabrice Normandin <normandf@mila.quebec>

--- cluv/cli/submit.py
+++ cluv/cli/submit.py
@@ -92,7 +92,7 @@ async def submit(
     program_args_str = shlex.join(program_args)
 
     # 6. Submit.
-    remote_cmd = f"bash -l -c '{env_prefix} sbatch --parsable {sbatch_args_str} {remote_job_script} {program_args_str}'"
+    remote_cmd = f"bash -l -c '{env_prefix} sbatch --parsable --chdir=~/{project_path} {sbatch_args_str} {remote_job_script} {program_args_str}'"
     console.print(
         f"Submitting job on [bold]{cluster}[/bold]: {job_script}"
         + (f" {sbatch_args_str}" if sbatch_args_str else "")

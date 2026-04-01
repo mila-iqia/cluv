845de842bd714dc46d5b86575acfdca45ed9e13f
Fabrice Normandin <normandf@mila.quebec>
2026-04-01 18:14:33 -0400

    Add a few notes in job.sh
    Signed-off-by: Fabrice Normandin <normandf@mila.quebec>

--- scripts/job.sh
+++ scripts/job.sh
@@ -12,6 +12,7 @@ echo "GIT_COMMIT=${GIT_COMMIT:?GIT_COMMIT is not set. Use 'cluv submit' to submi
 # echo "Job has been restarted $SLURM_RESTART_COUNT times."
 
 # Setup the repo in $SLURM_TMPDIR, so the code can change in the project without affecting the job.
+echo "Preparing the repo and virtual environment in $SLURM_TMPDIR"
 srun --ntasks-per-node=1 --ntasks=$SLURM_NNODES --input=all bash -e <<END
 cd $SLURM_TMPDIR
 git clone $project_root
@@ -21,9 +22,11 @@ exec uv sync
 END
 
 # Run the actual job command passed as an argument ('python main.py' for example)
+echo "Running command: $@"
 srun uv --directory=$SLURM_TMPDIR/cluv run "$@"
 
 # IDEA: Display a warning if there are files in $SLURM_TMPDIR that would be lost.
 
 # Copy results (if any) from the local storage back to the results dir (eg in $SCRATCH)
+echo "Copying logs from $SLURM_TMPDIR/cluv/logs to $project_root/logs"
 srun --ntasks-per-node=1 rsync --update --recursive $SLURM_TMPDIR/cluv/logs $project_root/logs

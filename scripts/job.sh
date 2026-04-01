dc2a8510db537eab09c98e822e0cc7d8b54e68c4
Fabrice Normandin <normandf@mila.quebec>
2026-04-01 18:11:47 -0400

    Fix bug in job.sh
    Signed-off-by: Fabrice Normandin <normandf@mila.quebec>

--- scripts/job.sh
+++ scripts/job.sh
@@ -15,8 +15,8 @@ echo "GIT_COMMIT=${GIT_COMMIT:?GIT_COMMIT is not set. Use 'cluv submit' to submi
 srun --ntasks-per-node=1 --ntasks=$SLURM_NNODES --input=all bash -e <<END
 cd $SLURM_TMPDIR
 git clone $project_root
-git checkout --detach $GIT_COMMIT
 cd $SLURM_TMPDIR/cluv
+git checkout --detach $GIT_COMMIT
 exec uv sync
 END
 

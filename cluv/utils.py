21940b4c28a16e60f79f2cfeeb911637a7722cdb
hvdbm <hugo.vdbm@icloud.com>
2026-04-15 09:12:34 -0400

    Fix hostname check in `current_cluster` (#14)
    * Fix hostname check in current_cluster function
    
    * Update hostname value check

--- cluv/utils.py
+++ cluv/utils.py
@@ -9,7 +9,7 @@ console = rich.console.Console(record=True, file=sys.stdout)
 
 
 def current_cluster() -> str | None:
-    if socket.gethostname().endswith(".mila.quebec"):
+    if socket.gethostname().endswith(".server.mila.quebec"):
         return "mila"
     if "CC_CLUSTER" in os.environ:
         return os.environ["CC_CLUSTER"]

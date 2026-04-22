d51e2ea56a9204c484e17c8235cbb42558658707
Hugo Vandenbroucke-Menu <hugo.vandenbroucke-menu@mila.quebec>
2026-04-22 16:29:33 -0400

    Refactor checkpointing logic to accept file path as argument and add command-line argument parsing for results directory

--- examples/simple-checkpoint/src/simple_checkpoint/__init__.py
+++ examples/simple-checkpoint/src/simple_checkpoint/__init__.py
@@ -7,31 +7,32 @@ The counter will be incremented every 5 seconds, and the new value will be writt
 from pathlib import Path
 from time import sleep
 import os
+import argparse
 import random
 
 # Get JOB_ID from environment variable, or generate a random one if not found (e.g., when running locally)
 rand = str(random.randint(1000, 9999))
 JOB_ID = os.environ.get("SLURM_JOB_ID", rand)
-FILE_PATH = Path("logs") / JOB_ID / "counter.txt"
+# FILE_PATH = Path("logs") / JOB_ID / "counter.txt"
 
 N_STEPS = 10
 
-def init() -> int:
-    if FILE_PATH.exists():
-        with open(FILE_PATH, "r") as f:
+def init(file_path: Path) -> int:
+    if file_path.exists():
+        with open(file_path, "r") as f:
             counter = int(f.read())
         print(f"Resuming from checkpoint. Counter value: {counter}\n")
     else:
         print("No checkpoint found. Starting from 0.")
-        FILE_PATH.parent.mkdir(parents=True, exist_ok=True)
+        file_path.parent.mkdir(parents=True, exist_ok=True)
         counter = 0
     return counter
 
-def foo(n: int) -> int:
+def foo(n: int, file_path: Path) -> int:
     n += 1
-    with open(FILE_PATH, "w") as f:
+    with open(file_path, "w") as f:
         f.write(str(n))
-    print(f"Counter value written to {FILE_PATH}: {n} \n")
+    print(f"Counter value written to {file_path}: {n} \n")
     sleep(5)
 
     # Simulate a random interruption to demonstrate checkpointing (10% chance to interrupt)
@@ -44,11 +45,17 @@ def foo(n: int) -> int:
 def main() -> None:
     print("Hello from simple-checkpoint!")
 
-    counter = init()
+    argparser = argparse.ArgumentParser(description="Simple checkpointing example.")
+    argparser.add_argument("--results-dir", default="logs", help="Directory to load/save experiments.")
+    args = argparser.parse_args()
+
+    file_path = Path(args.results_dir) / JOB_ID / "counter.txt"
+
+    counter = init(file_path)
 
     for i in range(N_STEPS):
         print(f"Step {i+1}/{N_STEPS}")
-        counter = foo(counter)
+        counter = foo(counter, file_path)
 
 if __name__ == "__main__":
     main()

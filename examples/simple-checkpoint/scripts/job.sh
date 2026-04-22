0df8036b0993396260bc028aea802c8722486cc1
Hugo Vandenbroucke-Menu <hugo.vandenbroucke-menu@mila.quebec>
2026-04-22 15:58:39 -0400

    Fix git clone path and adjust directory navigation in job script

--- examples/simple-checkpoint/scripts/job.sh
+++ examples/simple-checkpoint/scripts/job.sh
@@ -16,9 +16,10 @@ echo "GIT_COMMIT=${GIT_COMMIT:?GIT_COMMIT is not set. Use 'cluv submit' to submi
 echo "Preparing the repo and virtual environment in $SLURM_TMPDIR"
 srun --ntasks-per-node=1 --ntasks=$SLURM_NNODES --input=all bash -e <<END
 cd $SLURM_TMPDIR
-git clone $HOME/Desktop/cluv
-cd $SLURM_TMPDIR/cluv/examples/simple-checkpoint
+git clone /home/mila/v/vandenbh/Desktop/cluv
+cd $SLURM_TMPDIR/cluv
 git checkout --detach $GIT_COMMIT
+cd examples/simple-checkpoint
 exec uv sync
 END
 
@@ -27,7 +28,8 @@ echo "Running command: $@"
 # Note: This `--gres-flags=allow-task-sharing` is required to allow tasks on the same node to access
 # GPUs allocated to other tasks on that node. Without this flag, --gpus-per-task=1 would isolate
 # each task to only see its own GPU, which can cause some mysterious NCCL errors.
-srun --gres-flags=allow-task-sharing uv --directory=$SLURM_TMPDIR/$project_name run "$@"
+project_example="$SLURM_TMPDIR/cluv/examples/simple-checkpoint"
+srun --gres-flags=allow-task-sharing uv --directory=$project_example run "$@"
 
 # Copy results (if any) from the local storage back to the results dir (eg in $SCRATCH)
 echo "Copying logs from $SLURM_TMPDIR/$project_name/$results_path to $project_root/$results_path"

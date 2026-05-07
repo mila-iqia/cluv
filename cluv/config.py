006d928cd77a42ac43638d4c5389334b90ad63c3
Hugo Vandenbroucke-Menu <hugo.vandenbroucke-menu@mila.quebec>
2026-05-07 09:24:17 -0400

    Fix doc backlinks

--- cluv/config.py
+++ cluv/config.py
@@ -1,14 +1,4 @@
-"""Lightweight config system based on a section in the pyproject.toml file.
-
-Should at the very least contain:
-- a list of available clusters (hostnames).
-
-Ideally, should also contain:
-- A set of overrides for each cluster (which partition, gpu, account, etc to use).
-
-Stretch goal (might be useful):
-- Documentation links for each cluster (for LLMs to look at?)
-"""
+"""Lightweight config system based on a section in the pyproject.toml file."""
 
 from __future__ import annotations
 

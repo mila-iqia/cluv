ec43e7dc4816c7da24f64a3634c9f033f56e7e0c
Fabrice Normandin <normandf@mila.quebec>
2026-05-11 16:19:48 -0400

    Add ImageNet example
    Signed-off-by: Fabrice Normandin <normandf@mila.quebec>

---
+++ examples/imagenet/scripts/job.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=4G
+#SBATCH --time=0:05:00
+#SBATCH --output=logs/%j/slurm-%j.out
+
+# Run the job command passed as an argument when submitting the job ('python main.py' for example)
+echo "Running command: $@"
+srun uv run "$@"

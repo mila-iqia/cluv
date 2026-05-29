b7d924899f07b526f8acb2b4e11f774a86cdbbc1
Fabrice Normandin <normandf@mila.quebec>
2026-05-29 17:07:22 -0400

    Rename example
    Signed-off-by: Fabrice Normandin <normandf@mila.quebec>

--- examples/pytorch-example/src/pytorch_example/main.py
+++ examples/pytorch-example/src/pytorch_example/main.py
@@ -13,12 +13,14 @@ from dataclasses import dataclass
 
 import simple_parsing
 import torch
+import torch.backends
 import tqdm
 import wandb
 from torchvision.datasets import CIFAR10
 
 from cluv.config import current_cluster_config
 from cluv.job import current_job_info
+from cluv.utils import current_cluster
 
 
 @dataclass(frozen=True)
@@ -33,6 +35,21 @@ class Args:
 
 
 def main(args: Args | None = None):
+    cluster = current_cluster()
+    cuda_built = torch.backends.cuda.is_built()
+    cuda_avail = torch.cuda.is_available()
+    device_count = torch.cuda.device_count()
+
+    print(f"Run on cluster:       {cluster}")
+    print(f"PyTorch built with CUDA:         {cuda_built}")
+    print(f"PyTorch detects CUDA available:  {cuda_avail}")
+    print(f"PyTorch-detected #GPUs:          {device_count}")
+    if device_count == 0:
+        print("    No GPU detected.")
+    else:
+        for i in range(device_count):
+            print(f"    GPU {i}:      {torch.cuda.get_device_name(i)}")
+
     args = args or simple_parsing.parse(Args, description=__doc__)
 
     job_info = current_job_info()
@@ -56,6 +73,14 @@ def main(args: Args | None = None):
     assert cluster_info.datasets_path, "This example requires a datasets_path to be set."
     dataset = CIFAR10(cluster_info.datasets_path, download=False)
     print(dataset)
+
+    # model = torchvision.models.resnet18(num_classes=10)
+    # optimizer = torch.optim.SGD(model.parameters(), lr=0.1)
+    # TODO: Make this a distributed example, so that it can also run on Tamia and others with
+    # full-node job allocations.
+    # from torch.distributed.fsdp import FullyShardedDataParallel as FSDP
+    # from torch.nn.parallel import DistributedDataParallel
+    # model = DistributedDataParallel(model)
 
     for i in tqdm.tqdm(range(args.wait_duration_seconds), disable=(not sys.stdout.isatty())):
         # Some fake, loss that varies a bit between seeds and decreases over time.

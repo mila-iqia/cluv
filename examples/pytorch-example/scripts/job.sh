2886fd4a27f71b296f0457f618e663799f0eecad
Fabrice Normandin <normandf@mila.quebec>
2026-06-09 11:52:57 -0400

    Ignore trillium and trillium-gpu clusters for now
    Signed-off-by: Fabrice Normandin <normandf@mila.quebec>

--- examples/pytorch-example/scripts/job.sh
+++ examples/pytorch-example/scripts/job.sh
@@ -3,10 +3,6 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=2
 #SBATCH --time=0:15:00
-# NOTE: This will be overwritten by Cluv anyway. Can't be left empty, else the job submission will
-# fail on Trillium and Trillium-gpu, since it would be in $HOME which is apparently read-only from
-# compute nodes.
-#SBATCH --output=/scratch/your_username/slurm-%j.out
 
 # Minimal job script to validate the PyTorch setup.
 echo "hostname: $(hostname)"

bc20880220ec2dc8cf345121f9dc54b9b387862f
Fabrice Normandin <normandf@mila.quebec>
2026-06-25 12:02:10 -0400

    WIP: Adding a test for the Hydra example
    Signed-off-by: Fabrice Normandin <normandf@mila.quebec>

---
+++ tests/test_examples.py
@@ -0,0 +1,48 @@
+import subprocess
+from pathlib import Path
+
+import pytest
+from pytest_regressions.file_regression import FileRegressionFixture
+
+from cluv.remote import Remote
+
+from .test_integration import cluster, remote  # noqa: F401
+
+
+@pytest.mark.parametrize(
+    cluster.__name__,
+    [
+        "mila",
+        pytest.param(
+            "rorqual",
+            marks=pytest.mark.xfail(
+                reason="Rorqual might take a long time for the job to actually run."
+            ),
+        ),
+    ],
+    indirect=True,
+)
+@pytest.mark.slow
+async def test_hydra_example(
+    remote: Remote,  # noqa
+    monkeypatch: pytest.MonkeyPatch,
+    file_regression: FileRegressionFixture,
+) -> None:
+    """End-to-end: actually run the hydra example.
+
+    Requires an active SSH connection to the cluster and a clean git tree.
+    Also actually performs a `cluv sync` to that cluster.
+    """
+    repo_root = Path(__file__).parent.parent
+    monkeypatch.chdir(repo_root / "examples/hydra_example")
+
+    subprocess_result = subprocess.run(
+        f"uv run python main.py --multirun launcher=cluv cluster={remote.hostname} lr=0.1,0.2",
+        shell=True,
+        text=True,
+        # capture_output=True,
+        check=True,
+    )
+    file_regression.check(
+        subprocess_result.stdout or subprocess_result.stderr, extension=".stdout", encoding="utf-8"
+    )

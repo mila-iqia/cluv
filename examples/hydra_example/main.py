4c2a38b1f4462613c7ee19c18cd4da62be5daf12
Fabrice Normandin <normandf@mila.quebec>
2026-07-02 14:12:53 -0400

    Tweak the example
    Signed-off-by: Fabrice Normandin <normandf@mila.quebec>

--- examples/hydra_example/main.py
+++ examples/hydra_example/main.py
@@ -52,17 +52,30 @@ def main(config_dict: DictConfig):
     # Use the 'usual' way to get the Hydra output dir:
     output_dir = hydra.core.hydra_config.HydraConfig.get().runtime.output_dir
     print(f"Output directory: {output_dir}")
+    hydra_job_info = hydra.core.hydra_config.HydraConfig.get().job
+    print(f"Job info: {hydra_job_info}")
 
     # job_info = cluv.job.current_run_info()
-    run_info = cluv.job.current_run_info()
 
-    if run_info:
+    ### Using Cluv in your Python script
+    # Use cluv in your script to get where to get information that would otherwise depend on which
+    # cluster you are on or how the job was submitted.
+    #
+    # For example: Here we get these:
+    # 1. the path to the datasets folder on the current cluster, and
+    # 2. The 'run id' to use with Wandb for the current run.
+
+    if run_info := cluv.job.current_run_info():
         # Running on a Slurm cluster. Use the setting from the cluster config.
         print(f"Running on cluster {run_info.cluster}!")
         datasets_path = run_info.cluster_config.datasets_path
+        run_id = run_info.run_id
+        run_dir = Path(os.path.expandvars(run_info.results_path))
     else:
         # Not running on a Slurm cluster. Use the setting from the cluv config.
         datasets_path = cluv.config.get_cluv_config().datasets_path
+        run_id = None
+        run_dir = None
     if not datasets_path:
         raise ValueError(
             "A datasets_path must be set either in the config for this example to work."
@@ -70,19 +83,13 @@ def main(config_dict: DictConfig):
 
     datasets_path = Path(os.path.expandvars(datasets_path))
 
-    run_id = None
-    run_dir = None
-    if job_info := cluv.job.current_run_info():  # if we are in a Slurm job:
-        run_id = job_info.run_id
-        run_dir = Path(os.path.expandvars(job_info.results_path))
-
     wandb_run = wandb.init(
         project="cluv-example",
         name=run_id,
         id=run_id,
-        # dir=run_dir,
+        dir=run_dir.parent if run_dir else None,
         config={"config": dataclasses.asdict(config)}
-        | ({"job": dataclasses.asdict(job_info)} if job_info else {})
+        | ({"job": dataclasses.asdict(run_info)} if run_info else {})
         | {"env": {k: v for k, v in os.environ.items() if k.startswith("SLURM")}},
         resume="allow",
         # if using Hydra multirun to run multiple jobs in sequence, create a new run for each.
@@ -101,20 +108,32 @@ def main(config_dict: DictConfig):
     dataset = CIFAR10(datasets_path, download=False)
     print(dataset)
 
+    # TODO: Simulate resuming from a checkpoint.
+    checkpoint_file = run_dir / "checkpoint.txt"
+    if checkpoint_file.exists():
+        start_time = int(checkpoint_file.read_text()) + 1
+        print(f"Resuming from checkpoint {checkpoint_file} at step {start_time + 1}.")
+    else:
+        start_time = 0
+
+    # TODO: Make this a "real" torch distributed example that run on Tamia and other clusters with
+    # full-node job allocations.
     # model = torchvision.models.resnet18(num_classes=10)
     # optimizer = torch.optim.SGD(model.parameters(), lr=0.1)
-    # TODO: Make this a distributed example, so that it can also run on Tamia and others with
-    # full-node job allocations.
     # from torch.distributed.fsdp import FullyShardedDataParallel as FSDP
     # from torch.nn.parallel import DistributedDataParallel
     # model = DistributedDataParallel(model)
 
-    for i in tqdm.tqdm(range(config.job_duration_seconds), disable=(not sys.stdout.isatty())):
+    for i in tqdm.tqdm(
+        range(start_time, config.job_duration_seconds), disable=(not sys.stdout.isatty())
+    ):
+        random.seed(config.seed + i)
         # Some fake, loss that varies a bit between seeds and decreases over time.
         fake_loss = math.exp(-i / 10) + random.random() * 0.1
         time.sleep(1)
         wandb_run.log({"step": i, "loss": fake_loss})
-        # print(f"Step {i}: loss={fake_loss}")
+        logger.debug("Step {}: loss={}", i, fake_loss)
+        checkpoint_file.write_text(str(i))
 
     print(f"Job {run_id} is about to end.")
 

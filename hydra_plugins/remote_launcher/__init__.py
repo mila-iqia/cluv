0ec9e1484aeb42e58b40b96641cb5c3a6b2d1d89
Fabrice Normandin <normandf@mila.quebec>
2026-07-08 15:49:32 -0400

    Remove the leftover remote launcher plugin
    Signed-off-by: Fabrice Normandin <normandf@mila.quebec>

--- hydra_plugins/remote_launcher/__init__.py
+++
@@ -1,7 +0,0 @@
-from .remote_launcher_plugin import (
-    PatchedSlurmQueueConf,
-    RemoteSlurmLauncher,
-    RemoteSlurmQueueConf,
-)
-
-__all__ = ["RemoteSlurmLauncher", "PatchedSlurmQueueConf", "RemoteSlurmQueueConf"]
